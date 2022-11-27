a527a6774ade561bcc6fd30a46f5cd7bf4ebc19a
erblirabajrami <93322578+erblirabajrami@users.noreply.github.com>
2022-11-27 19:57:22 +0100

    Add files via upload
    Helpers

---
+++ DataContext.cs
@@ -0,0 +1,22 @@
+namespace MyWebApp.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Entities;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // in memory database used for simplicity, change to a real db for production applications
+        options.UseInMemoryDatabase("TestDb");
+    }
+
+    public DbSet<User> Users { get; set; }
+}
